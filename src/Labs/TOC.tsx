847aa2db9e956b7cab61e6ce66e99e244a1d6bbe
DongxiaoLi98 <darialddx@gmail.com>
2024-11-21 14:03:03 -0500

    _deploy

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -68,7 +68,7 @@ My GitHub
             </li>
 
             <li className="nav-item">
-                <a id="wd-github" href="https://kanbas-node-server-app-1-q5rm.onrender.com" className="nav-link"> Render</a>
+                <a id="wd-github" href="https://kanbas-node-server-app-1-q5rm.onrender.com" className="nav-link"> A5_Render</a>
             
             </li>
 
