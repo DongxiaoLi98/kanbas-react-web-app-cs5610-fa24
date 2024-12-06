16ccad6ee1b8e719284e10b6558f0e1ebae5b3e1
DongxiaoLi98 <darialddx@gmail.com>
2024-12-05 20:36:09 -0500

    a6_5.2

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -6,6 +6,19 @@ const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 
+export const findModulesForCourse = async (courseId: string) => {
+  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/modules`);
+  return response.data;
+};
+
+export const createModuleForCourse = async (courseId: string, module: any) => {
+  const response = await axiosWithCredentials.post(
+    `${COURSES_API}/${courseId}/modules`,
+    module
+  );
+  return response.data;
+}; 
+
 export const updateCourse = async (course: any) => {
   const { data } = await axiosWithCredentials.put(`${COURSES_API}/${course._id}`, course);
   return data;
@@ -26,20 +39,6 @@ export const createCourse = async (course: any) => {
 export const fetchAllCourses = async() => {
     const {data} = await axiosWithCredentials.get(COURSES_API);
     return data;
-};
-
-export const createModuleForCourse = async (courseId: string, module: any) => {
-    const response = await axios.post(
-      `${COURSES_API}/${courseId}/modules`,
-      module
-    );
-    return response.data;
-};
-
-export const findModulesForCourse = async (courseId: string) => {
-    const response = await axios
-      .get(`${COURSES_API}/${courseId}/modules`);
-    return response.data;
 };
 
 export const findAssignmentsForCourse = async (courseId: string) => {
